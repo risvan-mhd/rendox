dcf6bc8d07557885d5ba56e0c7860db9259b8392
Risvan Muhammad <risvan145k@gmail.com>
2026-05-10 19:18:29 +0530

    feat: add template variable extraction

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from docxtpl import DocxTemplate
+from pathlib import Path
+import zipfile
+
+
+def extract_template_variables(path: str | Path) -> set[str]:
+    """Extracts all {{ jinja2 }} variables from a .docx file."""
+    try:
+        return DocxTemplate(path).get_undeclared_template_variables()
+    except zipfile.BadZipFile:
+        raise ValueError(f"Invalid file: {str(path)!r}")
+
+
 def main():
-    print("Hello from rendox!")
+    test_file = Path("template.docx")
+    if test_file.exists():
+        variables = extract_template_variables(test_file)
+        print(f"Template variables: {variables}")
+
+    else:
+        print(
+            f"Place {test_file.as_posix()!r} to test {extract_template_variables.__name__}() function"
+        )
 
 
 if __name__ == "__main__":
